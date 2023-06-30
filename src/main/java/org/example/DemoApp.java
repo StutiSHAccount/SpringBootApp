2cb93d8b207d671643c5ffe6871fe2b94b336938
stuti github <stutigithub@gmail.com>
2023-06-30 13:54:24 +0530

    added pom and src files.

--- src/main/java/org/example/DemoApp.java
+++ src/main/java/org/example/DemoApp.java
@@ -1,11 +1,9 @@
-package org.example;
-
+package src.main.java.org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -13,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApp {
 
-    @GetMapping ("/get")
+    @GetMapping("/get")
     public String getMessage() {
         return "the server started";
     }
